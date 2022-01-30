f8f2cb4ef977b6a9482845edc1c307a7a3b1fb20
Donato Cardoso <donatocardoso11@gmail.com>
2022-01-30 11:46:30 -0300

    create api

---
+++ Api/ProvaCandidato.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace ProvaCandidato.Controllers
+{
+    [RoutePrefix("Health")]
+    public class HealthController : ApiController
+    {
+        [HttpGet, Route("Ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
